7c32942fe948df1283e12130f2e411c295b6856d
steven <ste85213@gmail.com>
2024-12-03 13:14:36 +0800

    修改Gmail前最正確一版

--- controllers/user.js
+++ controllers/user.js
@@ -21,6 +21,10 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  },
+  secure: true,
+  tls: {
+    rejectUnauthorized: false
   }
 })
 
